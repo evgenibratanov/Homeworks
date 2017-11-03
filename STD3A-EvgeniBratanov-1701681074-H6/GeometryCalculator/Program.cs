2a6576eadee597d7cf6714d3bfaccae0250c1292
evgenibratanov <evgeni_95@abv.bg>
2017-11-03 22:45:38 +0200

    My howerok for 6th week

---
+++ STD3A-EvgeniBratanov-1701681074-H6/GeometryCalculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option (triangle,square and radians :");
+            string thechoose = Console.ReadLine();
+            switch (thechoose)
+            {
+                case "triangle":
+                {
+                        Console.WriteLine("Enter side:");
+                        int side = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter height:");
+                        int height = int.Parse(Console.ReadLine());
+                        int answer = (side * height) / 2;
+                        Console.WriteLine($"The area of the triangle is = {answer}  ");
+
+
+
+                    }
+
+                
+                    break;
+                case "square":
+                        {
+                        Console.WriteLine("Enter side:");
+                        int side2 = int.Parse(Console.ReadLine());
+                        int face = side2 * side2;
+                        Console.WriteLine($"The are of the square is = {face}");
+
+
+                    }
+
+                        break;
+                case "radians":
+                    {
+                        Console.WriteLine("Enter the degrees you want to transfer in radians:");
+                        double degrees = double.Parse(Console.ReadLine());
+                        double radians = (Math.PI / 180) * degrees;
+                        Console.WriteLine($"Your degrees in radians are: {radians}");
+
+
+                    }
+                
+                    break;
+                
+                   
+
+
+            }
+        }
+    }
+}
---
+++ STD3A-EvgeniBratanov-1701681074-H6/GetMax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[3];
+            Console.WriteLine("Enter first number :");
+            num[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            num[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number :");
+            num[2] = int.Parse(Console.ReadLine());
+            int highest = ((num[0] > num[1] ? num[0] : num[1]) > num[2] ? (num[0] > num[1] ? num[0] : num[1]) : num[2]);
+            Console.WriteLine($"The biggest number is : {highest}");
+
+            
+        }
+    }
+}
